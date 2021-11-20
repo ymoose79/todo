5ef818e57020e63e21abe25e3c7eef1dffe69b6d
ymoose79 <justin.r.stock@icloud.com>
2021-11-20 13:06:21 -0500

    create/delete todo list

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,42 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	let toDo = '';
+	let toDoArr = ['get a job', 'mow the yard', 'wash the car', 'grocery'];
+	let todoList = [];
+
+	function checkButton() {
+		toDoArr = [toDo, ...toDoArr];
+		toDo = '';
+	}
+
+	function del(todoItems) {
+        console.log(todoList);
+		toDoArr = toDoArr.filter((value, i) => todoItems.indexOf(i) == -1)
+        todoList = []
+
+	}
+</script>
+
+<main>
+	<form on:submit|preventDefault={checkButton}>
+		<label for="name">Things To do:</label>
+		<input type="text" bind:value={toDo} />
+		<button>Add to list</button>
+	</form>
+	<form on:submit|preventDefault={del(todoList)}>
+		{#each toDoArr as todo, i}
+			<ul>
+				<input type="checkbox" bind:group={todoList} value={i} />
+				<label for="todo">{todo} </label>
+			</ul>
+		{/each}
+		<h1>{toDo}</h1>
+		<button>delete item</button>
+	</form>
+</main>
+
+<style>
+	main {
+		margin: 5rem 15rem;
+		font-size: 2em;
+	}
+</style>
