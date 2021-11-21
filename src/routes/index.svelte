1b4954857c10e9093ebb854fc18246f32c4119df
ymoose79 <justin.r.stock@icloud.com>
2021-11-21 10:54:26 -0500

    confirm now gives array

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,28 @@
 <script>
 	import Button from '../UI/Button.svelte';
 
-	let toDo = '';
-	let toDoArr = ['get a job', 'mow the yard', 'wash the car', 'grocery'];
+	let uid = 1;
+
+	let description = '';
+	let toDoArr = [
+		{ uid: uid++, description: 'get a job' },
+		{ uid: uid++, description: 'mow the yard' },
+		{ uid: uid++, description: 'wash the car' },
+		{ uid: uid++, description: 'grocery' }
+	];
 	let todoList = [];
 
 	function checkButton() {
-		toDoArr = [toDo, ...toDoArr];
-		toDo = '';
+		if (description === '') {
+			return;
+		}
+		const newTodo = {
+			uid: uid++,
+			description: description
+		}
+		toDoArr = [newTodo, ...toDoArr];
+		console.log(toDoArr);
+		description = '';
 	}
 
 	function del(todoItems) {
@@ -23,7 +38,7 @@
 		<div id="label">
 			<form on:submit|preventDefault={checkButton}>
 				<label for="name" />
-				<input id="input-field" type="text" bind:value={toDo} placeholder="thing to do" />
+				<input id="input-field" type="text" bind:value={description} placeholder="thing to do" />
 			</form>
 		</div>
 		<div class="button">
@@ -31,13 +46,13 @@
 		</div>
 	</div>
 	<form on:submit|preventDefault={del(todoList)}>
-		{#each toDoArr as todo, i}
+		{#each toDoArr as todo (todo.uid)}
 			<ul>
-				<input type="checkbox" bind:group={todoList} value={i} />
-				<label for="todo">{todo} </label>
+				<input type="checkbox"   />
+				<label for="todo">{todo.description} </label>
 			</ul>
 		{/each}
-		<h1>{toDo}</h1>
+		<h1>{description}</h1>
 		<button>delete item</button>
 	</form>
 </main>
@@ -55,14 +70,13 @@
 		border-radius: 1.5rem;
 		padding: 1rem;
 		width: 18rem;
-		border: .125rem solid rgba(179, 132, 201, .84);
+		border: 0.125rem solid rgba(179, 132, 201, 0.84);
 		-webkit-box-shadow: inset 0 0 8px rgba(0, 0, 0, 0.1), 0 0 16px rgba(0, 0, 0, 0.1);
 		-moz-box-shadow: inset 0 0 8px rgba(0, 0, 0, 0.1), 0 0 16px rgba(0, 0, 0, 0.1);
 		box-shadow: inset 0 0 8px rgba(0, 0, 0, 0.1), 0 0 16px rgba(0, 0, 0, 0.1);
 		padding: 15px;
 		background: rgba(255, 255, 255, 0.5);
 		margin: 0 0 10px 0;
-
 	}
 	.container {
 		display: flex;
@@ -71,6 +85,5 @@
 	#label {
 		margin-right: 3rem;
 		height: 1rem;
-		
 	}
 </style>
